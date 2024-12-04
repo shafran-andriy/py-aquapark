3bcb4dc7376c93fbdc4e2c3f1a602e32c2d8eff1
shafran <shafran.andriy@gmail.com>
2024-12-04 14:23:13 +0200

    Created all classes and methods

--- app/main.py
+++ app/main.py
@@ -2,24 +2,69 @@ from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int,
+                 max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner, name) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance, owner):
+        getattr(instance, self.protected_name)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError
+        if not (self.min_amount <= value <= self.max_amount):
+            raise ValueError
+        setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int,
+                 weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+
+    def __init__(self, name, limitation_class):
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor):
+        try:
+            # Instantiate the limitation class
+            validator = self.limitation_class()
+            # Validate each attribute
+            validator.age = visitor.age
+            validator.weight = visitor.weight
+            validator.height = visitor.height
+            return True
+        except (TypeError, ValueError):
+            return False
+
+
